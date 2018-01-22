c95e75c8d12184a20de7a334721f8c6972e8ae18
Lydia Young <lydiaryoung@gmail.com>
2018-01-21 19:01:50 -0700

    increments wins/losses and resets game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,21 @@ var losses = 0;	// # of times the user has failed to guess the letter correctly 
 var guessesLeft = 9; // # of guesses left. This will update
 var userGuesses = []; // an array of the letters that the user typed. Display these until the user either wins or loses.
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //array of letters the computer can choose from
+function resetGame() {
+	console.log("-----------------");
+	console.log("Game Reset");
+	console.log("# of wins: " + wins);
+	console.log("# of losses" + losses);
+	// Reset guessesLeft
+	guessesLeft = 9;
+	console.log("Guesses Left: " + guessesLeft)
+	// Reset userGuesses
+	userGuesses = [];
+	console.log(userGuesses)
+	// Reset random letter chosen by computer
+	var compChoice = letters[Math.floor(Math.random()*letters.length)];
+	console.log("Computer is thinking of the letter " + compChoice);
+}
 
 // Computer chooses a random letter:
 var compChoice = letters[Math.floor(Math.random()*letters.length)];
@@ -13,12 +28,36 @@ document.onkeyup = function(event) {
    	// make sure lowercase
     var userChoice = event.key.toLowerCase();
     console.log("The user guessed :" + userChoice);
-
+    // alert user if they have already made the same guess previously
+    // if (userChoice === userGuesses.find(userChoice)) {
+    // 	alert("You have already guessed that letter. Please choose a different letter.");
+    // };
     // Add guess to the array of guesses so far
     userGuesses.push(userChoice)
     console.log("Your guesses so far: " + userGuesses)
-    // alert user if they have already made the same guess previously
-    if (userChoice === userGuesses.find(userChoice)) {
-    	alert("You have already guessed that letter. Please choose a different letter.");
-    };
+
+	// Check user's guess against the computer's choice
+	// Win condition
+	if (userChoice === compChoice) {
+	   	wins++;
+	   	// Update wins counter on screen
+		// Reset game
+		resetGame();
+	}
+	// Guess is incorrect
+	else {
+    	guessesLeft--;
+    	// Update guess left on screen
+    	// Update guesses so far on screen    	
+	    }
+
+	// End game if used all guess attempts
+	if (guessesLeft === 0) {
+		losses++;
+		// Update loss counter on screen
+		// Reset game
+		resetGame();
+	}
+
+	
 }
